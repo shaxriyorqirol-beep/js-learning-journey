6d3af32617f3db1edcf71983ed7b195c9a41779c
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-23 18:27:05 +0500

    Add variables lesson

---
+++ js-lessons/01-variables.js
@@ -0,0 +1,16 @@
+let firstName = "Shaxriyor";
+let lastName = "Uzoqov";
+const age = 18;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+let newName = "Jahongir";
+newName = "Jahongirbek";
+
+const newAge = 20;
+// newAge = 21; // const berilgan o'zgaruvchiga yangi qiymat berib bo'lmaydi, bu xatolik beradi.
+
+console.log(newName);
+console.log(newAge);
