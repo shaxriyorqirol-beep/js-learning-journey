d3652d412e628b9d00a25b0f084361b4a362f106
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-24 09:21:01 +0500

    Add data types lesson

---
+++ js-lessons/02-data-types.js
@@ -0,0 +1,12 @@
+let firstName = "Shaxriyor";
+let lastName = "Uzoqov";
+const age = 18;
+let isProgrammer = true;
+let myFoods = ["Osh", "Manti", "Shashlik", "Somsa"];
+
+console.log(typeof firstName); // string
+console.log(typeof lastName); // string
+console.log(typeof age); // number
+console.log(typeof isProgrammer); // boolean
+console.log(typeof myFoods); // object
+console.log(Array.isArray(myFoods)); // true
