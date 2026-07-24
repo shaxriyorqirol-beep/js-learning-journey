566fbce742db6c8016756dedcf977faeda535125
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-24 14:20:19 +0500

    Add operators lesson

---
+++ js-lessons/03-operators.js
@@ -0,0 +1,24 @@
+// // Arithmetic operators
+// let a = 10;
+// let b = 3;
+
+// console.log(a + b); // 13
+// console.log(a - b); // 7
+// console.log(a * b); // 30
+// console.log(a / b); // 3.333...
+// console.log(a % b); // 0
+
+// // Assignment operators
+// console.log(5 == '5'); // true - qiymatni solishtiradi, lekin turini emas
+// console.log(5 === '5'); // false - turini ham solishtiradi
+// console.log(5 != '3'); // true - qiymatni solishtiradi, lekin turini emas
+// console.log(5 > '3'); // true - qiymatni solishtiradi, lekin turini emas
+// console.log(5 < '3'); // false - qiymatni solishtiradi, lekin turini emas
+
+// Logical operators
+let isProgrammer = true;
+let age = 18;
+
+console.log(isProgrammer && age > 18); // false - AND operator
+console.log(isProgrammer || age > 18); // true - OR operator
+console.log(!isProgrammer); // false - NOT operator
