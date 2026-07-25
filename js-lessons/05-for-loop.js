85c32b5c7692ce04c0c07dfd1ab73fbaa324858e
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-25 20:47:31 +0500

    Add for loop lessons

---
+++ js-lessons/05-for-loop.js
@@ -0,0 +1,14 @@
+// for (let son = 1; son <= 10; son++) {
+//     console.log(son);
+// }
+
+
+let name = "Shaxiyor";
+
+if (name === "Shaxriyor") {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i + ". Salom, mening ismim " + name + "!");
+    }
+} else {
+    console.log("Kechirasiz, siz Shaxriyor emassiz!");
+}
