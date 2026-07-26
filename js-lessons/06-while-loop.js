bde7f0275d25a64979954cf373c1fa6bbfd1148b
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-26 21:53:28 +0500

    Add while loop lesson 2

--- js-lessons/06-while-loop.js
+++ js-lessons/06-while-loop.js
@@ -2,8 +2,15 @@
 //     console.log('son ' + son);
 // }
 
-let son1 = 1;
-while (son1 <= 5) {
-    console.log('son ' + son1);
-    son1++;
+// let son1 = 1;
+// while (son1 <= 5) {
+//     console.log('son ' + son1);
+//     son1++;
+// }
+
+let  number = 2;
+
+while (number <= 10) {
+    console.log(number);
+    number+=2;
 }
