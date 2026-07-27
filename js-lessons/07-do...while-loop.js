c14562f27dddfc40dd6b58741084c5f059759027
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-27 23:45:41 +0500

    Add do...while loop 2 lesson

--- js-lessons/07-do...while-loop.js
+++ js-lessons/07-do...while-loop.js
@@ -11,9 +11,29 @@
 //     console.log("Hello");
 // } while (x < 10);
 
-let number = 1;
+// let number = 1;
+
+// do {
+//   console.log(number);
+//   number++;
+// } while (number <= 5);
+
+
+// let number = 1;
+
+// do {
+//     console.log(number)
+//     number++;
+// } while (number <= 10)
+
+// console.log("Finished !")
+
+let password = "1234";
+let input = "1111";
 
 do {
-  console.log(number);
-  number++;
-} while (number <= 5);
+  console.log("Wrong password");
+  input = "1234";
+} while (input !== password);
+
+console.log("Welcome!");
