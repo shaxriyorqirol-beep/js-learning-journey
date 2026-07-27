ae6fd7ed96501be63d24339f93d6cf690638b191
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-27 23:26:39 +0500

    Add do...while loop lesson

---
+++ js-lessons/07-do...while-loop.js
@@ -0,0 +1,19 @@
+// let x = 50;
+
+// while (x < 10) {
+//     console.log("Hello");
+//     break;
+// }
+
+// let x = 50;
+
+// do {
+//     console.log("Hello");
+// } while (x < 10);
+
+let number = 1;
+
+do {
+  console.log(number);
+  number++;
+} while (number <= 5);
