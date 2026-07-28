2242f72b2f127650bc0f3ccbd8fd69217d9fb916
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-28 13:34:57 +0500

    Add continue lesson

---
+++ js-lessons/09-continue.js
@@ -0,0 +1,31 @@
+// for (let son = 1; son <= 10; son++) {
+//     if (son === 3) {
+//         continue;
+//     }
+//     console.log(son);
+// }
+
+// let number = 1;
+
+// while (number <= 10) {
+//     if(number === 4) {
+//         number++;
+//         continue;
+//     }
+
+//     console.log(number);
+//     number++;
+// }
+
+let number = 1;
+
+while (number <= 5) {
+
+    number++;
+
+    if (number === 3) {
+        continue;
+    }
+
+    console.log(number);
+}
