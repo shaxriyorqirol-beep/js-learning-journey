ab6ef3329952f29ec3d2a47666fa67bb7f9fefa4
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-28 12:43:33 +0500

    Add break lesson

---
+++ js-lessons/08-break.js
@@ -0,0 +1,25 @@
+// for (let son = 1; son <= 10; son++) {
+
+//     if (son === 6) {
+//         break;
+//     }
+//     console.log(son)
+// }
+
+// let number = 1;
+
+// while (number <= 10) {
+//     if(number === 4) {
+//         break;
+//     }
+
+//     console.log(number);
+//     number++;
+// }
+
+for (let num = 20; num >= 10; num--) {
+    if(num === 15) {
+        break;
+    }
+    console.log(num);
+}
