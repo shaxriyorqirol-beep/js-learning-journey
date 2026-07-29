bb207b310bdc0278211e0ff12a9ee007e28bdae0
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-29 23:13:47 +0500

    Add function lesson

---
+++ js-lessons/10-functions.js
@@ -0,0 +1,41 @@
+// function helloSay() {
+//     console.log('Salom Shaxriyor!')
+// }
+
+// helloSay()
+
+// 1 - vazifa
+// function myCity() {
+//     console.log("Toshkent !")
+// }
+
+// myCity()
+// myCity()
+// myCity()
+
+// 2 - vazifa 
+// function myInfo() {
+//     console.log("My name is Shaxriyor.")
+//     console.log("I am learning JavaScript")
+//     console.log("I lova coding.")
+// }
+
+// myInfo()
+// myInfo()
+
+// 3 - vazifa 
+// console.log("Start !")
+
+// function welcome() {
+//     console.log("Welcome")
+// }
+
+// console.log("JavaScript")
+// welcome()
+// console.log("Finsh !")
+
+hello()
+
+function hello() {
+    console.log("Hello !")
+}
