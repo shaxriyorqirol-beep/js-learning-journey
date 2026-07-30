f57fcc669cf460f19486ef239d5d16bcaf402cc1
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-30 12:42:34 +0500

    Add scope lesson

---
+++ js-lessons/12-scope.js
@@ -0,0 +1,52 @@
+// Global scope
+// let name = "Shaxriyor"
+
+// function hello() {
+//     return name;
+// } 
+
+// let myName = hello()
+// console.log("Assalomu alaykum! ", myName);
+
+// Local scope
+// function age() {
+//     let myAge = 19;
+
+//     console.log(age);
+// }
+
+// age();
+// console.log(myAge);
+
+// 1 - vazifa
+// let city = "Toshkent";
+
+// function showCity() {
+//     console.log(city);
+// }
+
+// showCity();
+
+// 2 - vazifa
+// function test() {
+//     let color = "Blue";
+
+//     console.log(color);
+// }
+
+// test()
+// console.log(color)
+
+// 3 - vazifa
+let x = 100;
+
+function a() {
+    console.log(x);
+}
+
+function b() {
+    console.log(x);
+}
+
+a();
+b();
