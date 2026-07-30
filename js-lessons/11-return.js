854ab721c3c7d9c6fa4041e087f70df151879798
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-30 11:20:46 +0500

    Add return lesson

---
+++ js-lessons/11-return.js
@@ -0,0 +1,32 @@
+// function test() {
+//     console.log(1);
+
+//     return 50;
+
+//     console.log('2');
+// }
+
+// let x = test()
+// console.log(x)
+
+// 1 - vazifa
+// function getName() {
+//     console.log("Shaxriyor");
+
+//     return "Frontend";
+// }
+
+// let job = getName();
+
+// console.log(job);
+
+// 2 - vazifa
+function sum(a, b) {
+    console.log(a + b);
+
+    return a * b;
+}
+
+let result = sum(5, 4);
+
+console.log(result);
