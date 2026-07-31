cf14712b0668c4be3c7ab45a6511484a6c389149
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-08-01 00:42:08 +0500

    Add arrow function lesson 2

--- js-lessons/14-arrow-function.js
+++ js-lessons/14-arrow-function.js
@@ -22,5 +22,39 @@
 // let result = number(30, 30)
 // console.log(result)
 
-let salom = (name) => "Salom " + name;
-console.log(salom("Shaxriyor !"))
+// let salom = (name) => "Salom " + name;
+// console.log(salom("Shaxriyor !"))
+
+// let test = () => 100;
+// console.log(test())
+
+// let num = () => {
+//     return 100;
+// }
+// console.log(num())
+
+// let number = () => {
+//     100;
+// }
+
+// console.log(number())
+
+// 1 - vazifa 
+let hello = (name) => "Salom " + name;
+console.log(hello("Shaxriyor !"))
+
+// 2 - vazifa
+let double = (x) => x * 2;
+console.log(double(7));
+
+// 3 - vazifa
+let test = () => {
+    return "JavaScript";
+};
+console.log(test());
+
+// 4 - vazifa
+let sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(10, 20));
