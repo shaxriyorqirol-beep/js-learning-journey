c7433b85586a033aa895706080383e9c0614a0c0
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-07-31 22:46:07 +0500

    Add function expression.js

---
+++ js-lessons/13-function-expression.js
@@ -0,0 +1,38 @@
+// let hello = function() {
+//     console.log("hello !")
+// }
+
+// hello ()
+
+// 1 - vazifa 
+// let hello = function sayHello() {
+//     console.log("Hello !")
+// }
+// sayHello();
+
+// 2 - vazifa
+// let showName = function () {
+//     console.log("Shaxriyor")
+// }
+// showName()
+
+// 3 - vazifa
+// let sum = function () {
+//     return 10 + 20;
+// };
+
+// let result = sum();
+
+// console.log(result);
+
+hello();
+
+function hello() {
+    console.log("A");
+}
+
+let test = function () {
+    console.log("B");
+};
+
+test();
