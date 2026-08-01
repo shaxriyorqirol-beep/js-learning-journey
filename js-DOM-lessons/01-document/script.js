35b47fafa86e8d2308dd6ee59f95df990547b494
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-08-01 18:23:03 +0500

    feat: learn DOM document and textContent

---
+++ js-DOM-lessons/01-document/script.js
@@ -0,0 +1,43 @@
+// let title = document.getElementById("title").textContent;
+// let text = document.getElementById("text").textContent;
+
+// console.log(title);
+// console.log(text);
+
+// let name = document.getElementById("name");
+// let age = document.getElementById("age");
+// let title = document.getElementById("title");
+
+// console.log("Salom, " + name.textContent + " Siz " + age.textContent + " yoshdasiz " + "va " + title.textContent + " bo'lib ishlaysiz");
+
+// let title = document.getElementById("title");
+// title.textContent = "Backend";
+
+// console.log(title.textContent);
+
+// 1 - vazifa 
+let title = document.getElementById("title");
+console.log(title);
+console.log(title.textContent);
+
+// 2 - vazifa
+let title_two = document.getElementById("title_two");
+title_two.textContent = "Frontend";
+console.log(title_two.textContent);
+title_two.textContent = "Fullstack";
+console.log(title_two.textContent);
+
+// 3 - vazifa
+let age = document.getElementById("age");
+console.log(age.textContent);
+
+age.textContent = "20";
+console.log(age.textContent);
+
+// 4 - vazifa
+let title_three = document.getElementById("title_three");
+console.log(title_three.textContent);
+title_three.textContent = "DOM";
+console.log(title_three.textContent);
+title_three.textContent = "Frontend";
+console.log(title_three.textContent);
