1780c71ea9f9e8f2f9681be6491b6f501c36eb7e
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-08-02 14:23:39 +0500

    feat: learn DOM style manipulation

---
+++ js-DOM-lessons/02-style/script.js
@@ -0,0 +1,32 @@
+// let title = document.getElementById("title");
+// title.style.color = "red";
+
+// console.log(title.style.color);
+
+// 2 - vazifa uchun
+// let title = document.getElementById("title");
+// title.style.color = "green";
+
+// console.log(title.style.color);
+
+// 3 - vazifa uchun
+// let title = document.getElementById("title");
+// title.style.fontSize = "50px";
+
+// console.log(title.style.fontSize);
+
+
+// let title = document.getElementById("title");
+
+// title.style.color = "purple";
+// title.style.fontSize = "50px";
+
+// console.log(title.style.color);
+
+let title = document.getElementById("title");
+
+title.style.backgroundColor = "yellow";
+title.style.color = "black";
+
+console.log(title.style.backgroundColor);
+console.log(title.style.color);
