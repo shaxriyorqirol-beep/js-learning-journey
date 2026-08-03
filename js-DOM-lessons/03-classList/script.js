72e9ab5fbc5b267577c4b0a92346a6df292d06f3
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-08-03 14:15:28 +0500

    feat: learn DOM classList methods

---
+++ js-DOM-lessons/03-classList/script.js
@@ -0,0 +1,39 @@
+// let title = document.getElementById("title");
+// title.classList.add("active");
+
+// let title = document.getElementById("title");
+
+// console.log(title.classList);
+// title.classList.add("active");
+
+// let name = document.getElementById("name");
+
+// console.log(name.classList);
+// name.classList.remove("myname");
+// console.log(name.classList);
+
+// let name = document.getElementById("name");
+
+// console.log(name.classList);
+// name.classList.remove("myname");
+// console.log(name.classList);
+
+// let title = document.getElementById("title");
+
+// console.log(title.classList);
+// title.classList.toggle("active");
+
+// console.log(title.classList);
+// title.classList.toggle("active");
+// title.classList.toggle("active");
+// title.classList.toggle("active");
+// title.classList.toggle("active");
+// console.log(title.classList);
+
+let title = document.getElementById("title");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+    title.classList.toggle("active");
+})
+
