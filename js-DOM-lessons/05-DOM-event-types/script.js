97625ee8dcb83b8a80269c8e5a3892778540b38f
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-08-05 16:28:15 +0500

    feat: practice DOM mouse and keyboard events

---
+++ js-DOM-lessons/05-DOM-event-types/script.js
@@ -0,0 +1,92 @@
+// let title = document.getElementById("title");
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("dblclick", function() {
+//     title.textContent = "Hokage";
+// })
+
+// let title = document.getElementById("title");
+
+// title.addEventListener("mouseover", function() {
+//     title.textContent = "Hokage";
+// });
+
+// title.addEventListener("mouseout", function() {
+//     title.textContent = "Jirayajon";
+// });
+
+// let title = document.getElementById("title");
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("keydown", function() {
+//     title.textContent = "Hokage";
+// });
+
+// let title = document.getElementById("title");
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("keyup", function() {
+//     title.textContent = "Hokage";
+// }); 
+
+// 1 - vazifa
+// let title = document.getElementById("title");
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("click", function() {
+//     title.textContent = "Hokage 🍃";
+// });
+
+// btn.addEventListener("dblclick", function() {
+//     title.textContent = "Jiraya 🍃";
+// });
+
+
+// 2 - vazifa
+// let box = document.getElementById("box");
+
+// box.addEventListener("mouseover", function() {
+//     box.textContent = "Hokage 🍃";
+// });
+
+// box.addEventListener("mouseout", function() {
+//     box.textContent = "Xayr 👋";
+// });
+
+// 3 - vazifa
+// let title = document.getElementById("title");
+
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("keydown", function() {
+//     title.textContent = "Bosildi";
+// });
+
+// btn.addEventListener("keyup", function() {
+//     title.textContent = "Qo'yib yuborildi";
+// });
+
+// 4 - vazifa
+let title = document.getElementById("title");
+
+let btn = document.getElementById("btn");   
+
+btn.addEventListener("mouseover", function() {
+    title.textContent = "🔥";
+});
+
+btn.addEventListener("mouseout", function() {
+    title.textContent = "Jiraya";
+});
+
+btn.addEventListener("click", function() {
+    title.textContent = "Hokage";
+}); 
+
+btn.addEventListener("dblclick", function() {
+    title.textContent = "🍃 Legendary Sannin";
+});
