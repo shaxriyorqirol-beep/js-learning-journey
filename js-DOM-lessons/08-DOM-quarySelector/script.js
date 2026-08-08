4e34f8e2c0d83503d6b0f9cdfdafb67f5e6deaf2
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-08-08 18:30:49 +0500

    feat: Add DOM quaryselector lesson

---
+++ js-DOM-lessons/08-DOM-quarySelector/script.js
@@ -0,0 +1,50 @@
+// let title = document.querySelector("#title");
+// let text = document.querySelector(".text");
+// let btn = document.querySelector("#btn");
+
+// console.log(title.textContent);
+// console.log(text.textContent);
+
+// btn.textContent = "Button"
+// console.log(btn.textContent);
+
+// Vazifa
+// let title = document.querySelector("#title");
+// console.log(title.textContent)
+
+// let text = document.querySelector(".text");
+// console.log(text.textContent)
+
+// let btn = document.querySelector("button");
+// console.log(btn.textContent)
+
+// let text = document.querySelector(".text");
+// console.log(text.textContent);
+
+// 1 - vazifa 
+// let title = document.querySelector("#title");
+// title.textContent = "Hokage"
+
+// let text = document.querySelector(".text");
+// text.textContent = "Shnobi";
+
+// let btn = document.querySelector("button");
+// btn.textContent = "Boshladik"
+
+// btn.addEventListener("click", function () {
+//     console.log("Button tugma bosildi ")
+// })
+
+// 2 - vazifa 
+let title = document.querySelector("#title");
+title.textContent = "Hokage"
+
+let status = document.querySelector(".status");
+status.textContent = "Dars boshlandi"
+
+let btn = document.querySelector("button");
+btn.textContent = "Boshladik"
+
+btn.addEventListener("click", function () {
+    title.textContent = "Jiraya";
+});
