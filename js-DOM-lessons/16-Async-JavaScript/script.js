9f098d8c0fd7d728cd5ca57af788e84edc4a741a
shaxriyorqirol-beep <shaxriyorqirol@gmail.com>
2026-09-01 22:36:32 +0500

    Async JS lesson part 2

--- js-DOM-lessons/16-Async-JavaScript/script.js
+++ js-DOM-lessons/16-Async-JavaScript/script.js
@@ -23,12 +23,77 @@
 //     }, 2000)
 // })
 
-setTimeout(() => {
-    console.log("Salom !")
-}, 1000)
+// setTimeout(() => {
+//     console.log("Salom !")
+// }, 1000)
 
-function salomBer() {
-    console.log("Salom !")
+// function salomBer() {
+//     console.log("Salom !")
+// }
+
+// setTimeout(salomBer, 2000)
+
+// function salomBer() {
+//     console.log("Salom !")
+// }
+
+// function bajar(callback) {
+//     callback()
+// }
+
+// bajar(salomBer)
+
+// function ish(callback) {
+//     console.log("Ish boshlandi")
+
+//     callback()
+// }
+
+// function tugadi() {
+//     console.log("Ish tugadi")
+// }
+
+// ish(tugadi)
+
+// 
+
+// 
+// let choyQaynat = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Choy Qaynatildi !")
+//         }, 2000);
+//     })
+// }
+
+// async function choyIch() {
+//     let choy = await choyQaynat()
+
+//     console.log(choy)
+// }
+// choyIch()
+
+let choyQaynat = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let choyBorMi = false
+            
+            if (choyBorMi) {
+                resolve("Choy qaynatildi!")
+            } else {
+                reject("Choy tugagan, uzr!")
+            }
+        }, 2000);
+    })
 }
 
-setTimeout(salomBer, 2000)
+async function choyIch() {
+    try {
+        let choy = await choyQaynat()
+        console.log(choy)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+choyIch()
